6990055acfb9f0629b1ca86d2c43979593257e26
Ijeoma Igboagu <ijeonyi@gmail.com>
2024-07-27 19:03:58 +0100

    Update link in About component

--- comps/About.js
+++ comps/About.js
@@ -26,7 +26,7 @@ const About = () => {
 					</p>
 					<div className={styles.changeContainer}>
 						<a
-							href='https://drive.google.com/file/d/1UDxfCzibjfSD9M0-QIXxfUgWI5D6Vel_/view?usp=sharing'
+							href='https://drive.google.com/file/d/1Zv1p07NkkNfWbsFSq1_6yE2wHfW4UKC5/view?usp=sharing'
 							target='_blank'
 							rel='noopener noreferrer'>
 							<button className={styles.change} onClick={handleClick}>
